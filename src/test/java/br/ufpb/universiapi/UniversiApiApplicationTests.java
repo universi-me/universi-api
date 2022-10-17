0a570c58cdb7179048b67a726cde9c70f0a291a6
NathanJFA <nathan.farias@dcx.ufpb.br>
2022-10-17 19:55:03 -0300

    [nathan.farias] Iniciando projeto spring Universi.me

---
+++ src/test/java/br/ufpb/universiapi/UniversiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.universiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
