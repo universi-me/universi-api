6ab5306eef7bd337c93cf579c28c79f98433d28f
NathanJFA <nathan.farias@dcx.ufpb.br>
2022-10-17 20:24:33 -0300

    [nathan.farias] Criacao da classe Usuario

---
+++ src/main/java/br/ufpb/universiapi/entities/Usuario.java
@@ -0,0 +1,47 @@
+package br.ufpb.universiapi.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(){}
+
+    public Usuario(String name, String email, String password){
+        this.nome = name;
+        this.email = email;
+        this.senha = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
