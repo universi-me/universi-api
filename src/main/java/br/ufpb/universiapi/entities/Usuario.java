260a011d63cc8a152d59c8c72652115e90ab38c6
NathanJFA <nathan.farias@dcx.ufpb.br>
2022-10-17 20:30:54 -0300

    [nathan.farias] Adicionado o lombok

--- src/main/java/br/ufpb/universiapi/entities/Usuario.java
+++ src/main/java/br/ufpb/universiapi/entities/Usuario.java
@@ -1,8 +1,15 @@
 package br.ufpb.universiapi.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
     @Id
@@ -13,35 +20,10 @@ public class Usuario {
     private String email;
     private String senha;
 
-    public Usuario(){}
-
     public Usuario(String name, String email, String password){
         this.nome = name;
         this.email = email;
         this.senha = password;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
