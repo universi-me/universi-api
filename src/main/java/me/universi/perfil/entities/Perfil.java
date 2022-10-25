51b9e82ed9c8987317b7f6f4b698e1c5af5b9abb
NathanJFA <nathan.farias@dcx.ufpb.br>
2022-10-24 22:12:43 -0300

    feat#14: Adicionando atributos em Perfil

--- src/main/java/me/universi/perfil/entities/Perfil.java
+++ src/main/java/me/universi/perfil/entities/Perfil.java
@@ -1,11 +1,15 @@
 package me.universi.perfil.entities;
 
+import me.universi.competencia.entities.Competencia;
 import me.universi.link.entities.Link;
+import me.universi.perfil.enums.Sexo;
 import me.universi.usuario.entities.Usuario;
+import me.universi.grupo.entities.Grupo;
 
 import javax.persistence.*;
+import java.util.Collection;
 
-@Entity
+@Entity(name = "perfil")
 public class Perfil {
 
     @Id
@@ -20,13 +24,89 @@ public class Perfil {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_link")
     private Link link;
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "oerfil_competencia",
+            joinColumns = { @JoinColumn(name = "id_perfil") },
+            inverseJoinColumns = { @JoinColumn(name = "id_competencia") }
+    )
+    private Collection<Competencia> competencias;
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "perfil_grupo",
+            joinColumns = { @JoinColumn(name = "id_perfil") },
+            inverseJoinColumns = { @JoinColumn(name = "id_grupo") }
+    )
+    private Collection<Grupo> grupos;
+    @OneToMany(mappedBy = "link")
+    private Collection<Link> links;
+    @Column(name = "sexo")
+    @Enumerated(EnumType.STRING)
+    private Sexo sexo;
 
-    public Perfil(){
+    public Perfil(Long id, Usuario usuario, String bio, Link link, Collection<Competencia> competencias, Collection<Grupo> grupos, Collection<Link> links) {
+        this.id = id;
+        this.usuario = usuario;
+        this.bio = bio;
+        this.link = link;
+        this.competencias = competencias;
+        this.grupos = grupos;
+        this.links = links;
     }
 
-    public Perfil(Usuario usuario, String bio) {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
         this.bio = bio;
     }
 
+    public Link getLink() {
+        return link;
+    }
+
+    public void setLink(Link link) {
+        this.link = link;
+    }
+
+    public Collection<Competencia> getCompetencias() {
+        return competencias;
+    }
+
+    public void setCompetencias(Collection<Competencia> competencias) {
+        this.competencias = competencias;
+    }
+
+    public Collection<Grupo> getGrupos() {
+        return grupos;
+    }
+
+    public void setGrupos(Collection<Grupo> grupos) {
+        this.grupos = grupos;
+    }
+
+    public Collection<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(Collection<Link> links) {
+        this.links = links;
+    }
 }
