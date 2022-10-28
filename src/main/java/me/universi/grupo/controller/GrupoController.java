cb997c03191e6e21a2c45e109d645d4ae62dd231
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2022-10-28 12:44:11 -0300

    getmapping para Grupo

--- src/main/java/me/universi/grupo/controller/GrupoController.java
+++ src/main/java/me/universi/grupo/controller/GrupoController.java
@@ -3,19 +3,36 @@ package me.universi.grupo.controller;
 import me.universi.grupo.entities.Grupo;
 import me.universi.grupo.repositories.GrupoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
-@RestController
+@Controller
 public class GrupoController
 {
     @Autowired
     public GrupoRepository grupoRepository;
 
+    // mapaear tudo exceto, /css, /js, /img, /favicon.ico, comflita com static resources do Thymeleaf
+    @GetMapping(value = {"{url:(?!css$|js$|img$|favicon.ico$).*}/**"})
+    public String grupo_handler(HttpServletRequest request, HttpSession session, ModelMap map)
+    {
+        String requestSt = request.getRequestURI();
+        System.out.println("path: "+ requestSt);
+
+        // TODO, popular página grupo
+
+        return "grupo";
+    }
+
     // http://localhost:8080/projeto/criar?nome=teste&descricao=teste2
     @RequestMapping("/grupo/criar")
+    @ResponseBody
     public String create(@RequestParam("nome") String nome, @RequestParam("descricao") String descricao)
     {
         Grupo grupoNew = new Grupo();
@@ -25,6 +42,7 @@ public class GrupoController
 
     // http://localhost:8080/projeto/remover?id=1
     @RequestMapping("/grupo/remover")
+    @ResponseBody
     public String remove(@RequestParam("id") Long id)
     {
         try {
@@ -40,8 +58,9 @@ public class GrupoController
     }
 
     // http://localhost:8080/projeto/obter?id=1
-    @RequestMapping("/grupo/obter")
-    public Grupo get(@RequestParam("id") Long id)
+    @RequestMapping("/grupo/obter/{id}")
+    @ResponseBody
+    public Grupo get(@PathVariable Long id)
     {
         try {
             Grupo proj = grupoRepository.findById(id).get();
@@ -53,6 +72,7 @@ public class GrupoController
 
     // http://localhost:8080/projeto/listar
     @RequestMapping("/grupo/listar")
+    @ResponseBody
     public List<Grupo> getlist()
     {
         List<Grupo> ret = grupoRepository.findAll();
