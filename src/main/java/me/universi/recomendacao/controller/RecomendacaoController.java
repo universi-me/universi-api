450810acb2a843f7ed7e2b2dc7fd865f76d067d1
NathanJFA <nathan.farias@dcx.ufpb.br>
2022-10-29 17:34:15 -0300

    FEAT#84: Criação do mapeamento em Recomendação

---
+++ src/main/java/me/universi/recomendacao/controller/RecomendacaoController.java
@@ -0,0 +1,86 @@
+package me.universi.recomendacao.controller;
+
+
+import me.universi.grupo.entities.Grupo;
+import me.universi.recomendacao.entities.Recomendacao;
+import me.universi.usuario.entities.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import me.universi.recomendacao.service.RecomendacaoService;
+
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class RecomendacaoController {
+    @Autowired
+    public RecomendacaoService recomendacaoService;
+
+
+    // http://localhost:8080/recomendacao/criar?origem=2&destino=4&descricao=testeDescricao&competencia=4
+    @RequestMapping("/recomendacao/criar")
+    @ResponseBody
+    public String create(@RequestParam("origem") Long idOrigem, @RequestParam("destino") Long idDestino, @RequestParam("descricao") String descricao, @RequestParam("competencia") Long id) {
+        Recomendacao recomendacoNew = new Recomendacao();
+        //TODO - SETAR ATRIBUTOS NO OBJETO
+        //TODO - TRATAMENTO DE EXCEÇÕES
+        recomendacaoService.save(recomendacoNew);
+        return "Recomendacao criada: " + recomendacoNew.toString();
+    }
+    // http://localhost:8080/recomendacao/atualizar?id=5&4&descricao=testeDescricao
+    @RequestMapping("/recomendacao/atualizar")
+    @ResponseBody
+    public String create(@RequestParam("id") Long id, @RequestParam("descricao") String descricao) {
+        try{
+            Recomendacao recomendacao = recomendacaoService.findById(id);
+            //TODO - CRIAR VALIDAÇÃO DE PARÂMETROS
+            recomendacao.setDescricao(descricao);
+            recomendacaoService.save(recomendacao);
+            return "Recomendacao atualizada: " + recomendacao.toString();
+        }catch(EntityNotFoundException e){
+            return "Recomendação não encontrada";
+        }
+    }
+
+    // http://localhost:8080/recomendacao/remover?id=1
+    @RequestMapping("/recomendacao/remover")
+    @ResponseBody
+    public String remove(@RequestParam("id") Long id) {
+        try {
+            Recomendacao recomendacao = recomendacaoService.findById(id).get();
+            if (recomendacao != null) {
+                recomendacaoService.delete(recomendacao);
+                return "Recomendação Removida: " + recomendacao.toString();
+            }
+        } catch (EntityNotFoundException e) {
+            return "Recomendação não encontrada";
+        }
+        return "Falha ao remover";
+    }
+
+    // http://localhost:8080/recomendacao/obter?id=1
+    @RequestMapping("/recomendacao/obter/{id}")
+    @ResponseBody
+    public Recomendacao get(@PathVariable Long id) {
+        try {
+            Recomendacao recomendacao = recomendacaoService.findById(id).get();
+            return recomendacao;
+        } catch (EntityNotFoundException e) {
+            return null;
+        }
+    }
+
+    // http://localhost:8080/recomendacao/listar
+    @RequestMapping("/recomendacao/listar")
+    @ResponseBody
+    public List<Recomendacao> getlist() {
+        List<Recomendacao> recs = recomendcaoService.findAll();
+        return recs;
+    }
+}
