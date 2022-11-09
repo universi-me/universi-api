214893528077eac96edb36d96bb38e7b844838ef
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2022-11-09 20:45:24 -0300

    Grupo: add/remover participante, subgrupo em Services

--- src/main/java/me/universi/grupo/services/GrupoService.java
+++ src/main/java/me/universi/grupo/services/GrupoService.java
@@ -2,6 +2,7 @@ package me.universi.grupo.services;
 
 import me.universi.grupo.entities.Grupo;
 import me.universi.grupo.repositories.GrupoRepository;
+import me.universi.perfil.entities.Perfil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +12,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class GrupoService
-{
+public class GrupoService {
     @Autowired
     private GrupoRepository grupoRepository;
 
-    public Grupo findById(Long id)
-    {
+    public Grupo findById(Long id) {
         Optional<Grupo> grupoOptional = grupoRepository.findById(id);
         if(grupoOptional.isPresent()){
             return grupoOptional.get();
@@ -26,8 +25,7 @@ public class GrupoService
         }
     }
 
-    public Grupo findByNickname(String nickname)
-    {
+    public Grupo findByNickname(String nickname) {
         Optional<Grupo> grupoOptional = grupoRepository.findByNickname(nickname);
         if(grupoOptional.isPresent()){
             return grupoOptional.get();
@@ -36,8 +34,7 @@ public class GrupoService
         }
     }
 
-    public void adicionarSubgrupo(Grupo grupo, Grupo sub)
-    {
+    public void adicionarSubgrupo(Grupo grupo, Grupo sub) {
         Collection<Grupo> grupoArr = grupo.getSubGrupos();
         if(grupoArr == null) {
             grupoArr = new ArrayList<Grupo>();
@@ -47,20 +44,46 @@ public class GrupoService
         this.save(grupo);
     }
 
-    public void save(Grupo grupo)
-    {
+    public void removerSubgrupo(Grupo grupo, Grupo sub) {
+        Collection<Grupo> grupoArr = grupo.getSubGrupos();
+        if(grupoArr == null) {
+            grupoArr = new ArrayList<Grupo>();
+        }
+        grupoArr.remove(sub);
+        grupo.setSubGrupos(grupoArr);
+        this.save(grupo);
+    }
+
+    public void adicionarParticipante(Grupo grupo, Perfil perfil) {
+        Collection<Perfil> participantesArr = grupo.getParticipantes();
+        if(participantesArr == null) {
+            participantesArr = new ArrayList<Perfil>();
+        }
+        participantesArr.add(perfil);
+        grupo.setParticipantes(participantesArr);
+        this.save(grupo);
+    }
+
+    public void removerParticipante(Grupo grupo, Perfil perfil) {
+        Collection<Perfil> participantesArr = grupo.getParticipantes();
+        if(participantesArr == null) {
+            participantesArr = new ArrayList<Perfil>();
+        }
+        participantesArr.remove(perfil);
+        grupo.setParticipantes(participantesArr);
+        this.save(grupo);
+    }
+
+    public void save(Grupo grupo) {
         grupoRepository.save(grupo);
     }
 
-    public void delete(Grupo grupo)
-    {
+    public void delete(Grupo grupo) {
         grupoRepository.delete(grupo);
     }
 
-    public List<Grupo> findAll()
-    {
+    public List<Grupo> findAll() {
         return grupoRepository.findAll();
     }
-
 
 }
