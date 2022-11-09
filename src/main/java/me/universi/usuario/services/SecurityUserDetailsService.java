c8eb78e86ecc17bddf1643837970044daea2e526
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2022-11-09 19:05:51 -0300

    passwordEncoder para services do Usuario

--- src/main/java/me/universi/usuario/services/SecurityUserDetailsService.java
+++ src/main/java/me/universi/usuario/services/SecurityUserDetailsService.java
@@ -4,18 +4,18 @@ import me.universi.usuario.entities.Usuario;
 import me.universi.usuario.enums.Autoridade;
 import me.universi.usuario.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class SecurityUserDetailsService implements UserDetailsService {
     @Autowired
     private UsuarioRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Usuario> usuario = userRepository.findByEmail(username);
@@ -29,20 +29,23 @@ public class SecurityUserDetailsService implements UserDetailsService {
         if (user==null) {
             throw new Exception("Usuario está vazio!");
         }
-        if (user.getEmail()==null || user.getEmail().length()==0 || !user.getEmail().contains("@")) {
-            throw new Exception("Verifique o campo Email!");
-        }
-        if (user.getNome()==null || user.getNome().length()==0) {
-            throw new Exception("Verifique o campo Nome!");
-        }
-        if (user.getSenha()==null || user.getSenha().length()==0) {
-            throw new Exception("Verifique o campo Senha!");
-        }
-        Optional<Usuario> usuario = userRepository.findByEmail(user.getEmail());
-        if (usuario.isPresent()) {
-            throw new Exception("Usuário com email \""+user.getEmail()+"\" já esta cadastrado!");
-        }
         user.setAutoridade(Autoridade.ROLE_USER);
-        userRepository.save((Usuario) user);
+        userRepository.save((Usuario)user);
     }
+
+    public String codificarSenha(String senha) {
+        return passwordEncoder.encode(senha);
+    }
+
+    public boolean usernameExiste(String username) {
+        try {
+            if(loadUserByUsername(username) != null) {
+                return true;
+            }
+        }catch (UsernameNotFoundException e){
+            return false;
+        }
+        return false;
+    }
+
 }
