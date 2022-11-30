1d824ea3ce737c35312e52d81fd705a18b7495f2
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2022-11-30 15:03:57 -0300

    verificar nome do arquivo

--- src/main/java/me/universi/imagem/controller/ImagemController.java
+++ src/main/java/me/universi/imagem/controller/ImagemController.java
@@ -49,8 +49,8 @@ public class ImagemController {
     @GetMapping(value = "/img/imagem/{imagem}", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<InputStreamResource> obterImageEmDisco(HttpServletResponse response, @PathVariable("imagem") String nomeImagem) throws IOException {
         try {
-            String filename = nomeImagem.replaceAll(".", "").replaceAll("/", "").replaceAll("jpg", ".jpg");
-            File initialFile = new File(env.getProperty("DIRETORIO_DA_IMAGEM") + "imagem_" + filename);
+            String filename = nomeImagem.replaceAll(".", "").replaceAll("/", "").replaceAll("jpg", "");
+            File initialFile = new File(env.getProperty("DIRETORIO_DA_IMAGEM") + "imagem_" + filename + ".jpg");
             InputStream targetStream = new FileInputStream(initialFile);
             return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(new InputStreamResource(targetStream));
         }catch (Exception e) {
