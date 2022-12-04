f53baa90b0cf6ee464d8ab7b87943d3213886e6c
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2022-12-04 18:32:25 -0300

    Evitar conflito na classe Sys

--- src/main/java/me/universi/Sys.java
+++ src/main/java/me/universi/Sys.java
@@ -7,14 +7,16 @@ import me.universi.grupo.enums.GrupoTipo;
 import me.universi.grupo.exceptions.GrupoException;
 import me.universi.grupo.services.GrupoService;
 import me.universi.perfil.entities.Perfil;
-import me.universi.perfil.services.PerfilService;
 import me.universi.usuario.entities.Usuario;
 import me.universi.perfil.repositories.PerfilRepository;
 
+import me.universi.usuario.enums.Autoridade;
 import me.universi.usuario.services.UsuarioService;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -31,7 +33,7 @@ import java.util.Collection;
 public class Sys {
 
     @Autowired
-    public PerfilService perfilService;
+    public PerfilRepository perfilRepository;
     @Autowired
     public UsuarioService usuarioService;
     @Autowired
@@ -40,7 +42,7 @@ public class Sys {
     public GrupoService grupoService;
 
     public static void main(String [] args) {
-        System.out.println("H2 ativo na http://localhost:8080/h2-console");
+        System.out.println("H2 ativo na http://localhost/h2-console");
         SpringApplication.run(Sys.class, args);
     }
 
@@ -49,5 +51,94 @@ public class Sys {
         return "index";
     }
 
+    public Perfil random_perfil(String nome) {
+        Usuario userNew = new Usuario(nome, "test@email.com", usuarioService.codificarSenha("senha"));
+        try {
+            usuarioService.createUser(userNew);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
+        userNew.setNome(userNew.getNome()+"_"+userNew.getId());
+
+        Competencia competenciaNew = new Competencia();
+        competenciaNew.setNome("Java - admin"+userNew.getId());
+        competenciaNew.setDescricao("Sou top em java - admin"+userNew.getId());
+        competenciaService.save(competenciaNew);
+
+        Competencia competenciaNew1 = new Competencia();
+        competenciaNew1.setNome("Java - admin 1"+userNew.getId());
+        competenciaNew1.setDescricao("Sou top em java - admin 1"+userNew.getId());
+        competenciaService.save(competenciaNew1);
+
+        Perfil admin_perfil = userNew.getPerfil();
+        admin_perfil.setBio("Bio - admin_perfil"+userNew.getId());
+
+        Collection<Competencia> competencias = new ArrayList<Competencia>();
+        competencias.add(competenciaNew);
+        competencias.add(competenciaNew1);
+        admin_perfil.setCompetencias(competencias);
+
+
+        return admin_perfil;
+    }
+
+    @GetMapping("/popular")
+    String popular() throws GrupoException {
+        Grupo ufpb_grupo = grupoService.findFirstByNickname("ufpb");
+
+        if(ufpb_grupo != null) {
+            return "redirect:/";
+        }
+
+        Perfil admin_perfil = random_perfil("perfil_admin");
+        perfilRepository.save(admin_perfil);
+
+        Perfil perfil_1 = random_perfil("perfil_1");
+        perfilRepository.save(perfil_1);
+
+        Perfil perfil_2 = random_perfil("perfil_2");
+        perfilRepository.save(perfil_2);
+
+        Grupo novoGrupo = new Grupo();
+        novoGrupo.setNickname("ufpb");
+        novoGrupo.setTipo(GrupoTipo.INSTITUICAO);
+        novoGrupo.setAdmin(admin_perfil);
+        novoGrupo.setDescricao("Grupo da Instituição da UFPB");
+        novoGrupo.setNome("UFPB");
+
+        novoGrupo.setGrupoRoot(true);
+        novoGrupo.setPodeCriarGrupo(true);
+
+        grupoService.adicionarParticipante(novoGrupo, perfil_1);
+        grupoService.adicionarParticipante(novoGrupo, perfil_2);
+
+        Grupo novoGrupo2 = new Grupo();
+        novoGrupo2.setTipo(GrupoTipo.CAMPUS);
+        novoGrupo2.setNickname("campus4");
+        novoGrupo2.setAdmin(admin_perfil);
+        novoGrupo2.setDescricao("Grupo do Campus IV");
+        novoGrupo2.setNome("Campus IV");
+
+        grupoService.adicionarSubgrupo(novoGrupo, novoGrupo2);
+
+        return "redirect:/";
+    }
+
+    @Bean
+    InitializingBean sendDatabase() {
+        return () -> {
+            if(!usuarioService.usernameExiste("admin")) {
+                System.out.println("Criando usuário: admin:admin");
+                Usuario userAdmin = new Usuario("admin", null, usuarioService.codificarSenha("admin"));
+                try {
+                    usuarioService.createUser(userAdmin);
+                    userAdmin.setAutoridade(Autoridade.ROLE_ADMIN);
+                    usuarioService.save(userAdmin);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+    }
 }
