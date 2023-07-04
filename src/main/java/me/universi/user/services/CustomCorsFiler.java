64289b13373ee9bc246778c8a7d4cf65c212395c
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2023-07-04 15:16:16 -0300

    add custom CORS

---
+++ src/main/java/me/universi/user/services/CustomCorsFiler.java
@@ -0,0 +1,23 @@
+package me.universi.user.services;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+public class CustomCorsFiler extends CorsFilter {
+
+    public CustomCorsFiler() {
+        super(configurationSource());
+    }
+
+    private static UrlBasedCorsConfigurationSource configurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern ("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
