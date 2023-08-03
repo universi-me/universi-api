32b4ef844cf0c2b9eda0c9b76813ca27f24640b7
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2023-08-03 09:14:34 -0300

    Merge branch 'development' into feature#411

---
+++ src/main/java/me/universi/profile/controller/PerfilController.java
@@ -0,0 +1,98 @@
+package me.universi.profile.controller;
+
+import me.universi.api.entities.Response;
+import me.universi.competence.services.CompetenceTypeService;
+import me.universi.group.services.GroupService;
+import me.universi.profile.entities.Profile;
+import me.universi.profile.enums.Gender;
+import me.universi.profile.exceptions.PerfilException;
+import me.universi.profile.services.PerfilService;
+import me.universi.user.entities.User;
+import me.universi.user.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api")
+public class PerfilController {
+    @Autowired
+    public UserService userService;
+
+    @Autowired
+    public PerfilService perfilService;
+
+    @Autowired
+    public GroupService grupoService;
+    @Autowired
+    public CompetenceTypeService competenciaTipoService;
+
+    @PostMapping(value = "/perfil/editar", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Response perfil_editar(@RequestBody Map<String, Object> body) {
+
+        Response resposta = new Response(); // default
+
+        try {
+
+            String perfilId = (String)body.get("perfilId");
+            if(perfilId == null) {
+                throw new Exception("Parametro perfilId é nulo.");
+            }
+
+            String nome         = (String)body.get("nome");
+            String sobrenome    = (String)body.get("sobrenome");
+            String imagem       = (String)body.get("imagemUrl");
+            String bio          = (String)body.get("bio");
+            String sexo         = (String)body.get("sexo");
+
+            Profile profileAtual = perfilService.findFirstById(perfilId);
+            if(profileAtual == null) {
+                throw new PerfilException("Perfil não encontrado.");
+            }
+
+            if(!userService.usuarioDonoDaSessao(profileAtual.getUsuario())) {
+
+                User userSession = userService.getUserInSession();
+                if(!userService.isContaAdmin(userSession)) {
+                    throw new PerfilException("Você não tem permissão para editar este perfil.");
+                }
+            }
+
+            if(nome != null) {
+                profileAtual.setFirstname(nome);
+            }
+            if(sobrenome != null) {
+                profileAtual.setLastname(sobrenome);
+            }
+            if(imagem != null && imagem.length()>0) {
+                profileAtual.setImage(imagem);
+            }
+            if(bio != null) {
+                profileAtual.setBio(bio);
+            }
+            if(sexo != null) {
+                profileAtual.setSexo(Gender.valueOf(sexo));
+            }
+
+            perfilService.save(profileAtual);
+
+            userService.atualizarUsuarioNaSessao();
+
+            resposta.message = "As Alterações foram salvas com sucesso.";
+            resposta.success = true;
+
+        } catch (Exception e) {
+            resposta.message = e.getMessage();
+        }
+
+        return resposta;
+    }
+}
