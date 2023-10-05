e67d11537bc195536005816811d8ab395e5fbf1c
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-10-05 17:57:19 -0300

    fix: prevent double assign

--- src/main/java/me/universi/capacity/entidades/Folder.java
+++ src/main/java/me/universi/capacity/entidades/Folder.java
@@ -8,6 +8,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -84,7 +85,8 @@ public class Folder {
     @JoinTable(
         name = "users_folders",
         joinColumns = @JoinColumn(name = "folder_id"),
-        inverseJoinColumns = @JoinColumn(name = "profile_id")
+        inverseJoinColumns = @JoinColumn(name = "profile_id"),
+        uniqueConstraints = { @UniqueConstraint(columnNames = {"folder_id", "profile_id"}) }
     )
     @JsonIgnore
     private Collection<Profile> assignedUsers;
