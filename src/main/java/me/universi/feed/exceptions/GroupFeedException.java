2573ad5219274be703796e84bdb4c7e3cecacafb
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2023-12-14 12:59:51 -0300

    change: adjust exception

--- src/main/java/me/universi/feed/exceptions/GroupFeedException.java
+++ src/main/java/me/universi/feed/exceptions/GroupFeedException.java
@@ -1,9 +1,5 @@
 package me.universi.feed.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class GroupFeedException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
