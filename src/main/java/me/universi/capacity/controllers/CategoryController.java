cba8e758849b004311ec60fd82f48e414bd59d4d
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-12-19 09:22:27 -0300

    change: create CategoryController
    * move Category-related endpoints from CapacityController to CategoryController
    * changed "/api/capacity/categories" endpoint to "/api/capacity/category/all"

---
+++ src/main/java/me/universi/capacity/controllers/CategoryController.java
@@ -0,0 +1,180 @@
+package me.universi.capacity.controllers;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.universi.api.entities.Response;
+import me.universi.capacity.entidades.Category;
+import me.universi.capacity.exceptions.CapacityException;
+import me.universi.capacity.service.CapacityService;
+import me.universi.user.services.UserService;
+
+@RestController
+@RequestMapping("/api/capacity/category")
+public class CategoryController {
+    private final CapacityService capacityService;
+
+    public CategoryController(CapacityService capacityService) {
+        this.capacityService = capacityService;
+    }
+
+    @GetMapping("/all")
+    public Response list() {
+        return Response.buildResponse(response -> {
+            response.body.put("categories", capacityService.getAllCategories());
+        });
+    }
+
+    @PostMapping(value = "/contents", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response contentsByCategory(@RequestBody Map<String, Object> body) {
+        return Response.buildResponse(response -> {
+
+            Object categoryId = body.get("id");
+            if(categoryId == null || String.valueOf(categoryId).isEmpty()) {
+                throw new CapacityException("ID da categoria não informado.");
+            }
+
+            response.body.put("contents", capacityService.findContentsByCategory(String.valueOf(categoryId)));
+        });
+    }
+
+    @PostMapping(value = "/folders", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response foldersByCategory(@RequestBody Map<String, Object> body) {
+        return Response.buildResponse(response -> {
+
+            Object categoryId = body.get("id");
+            if(categoryId == null || String.valueOf(categoryId).isEmpty()) {
+                throw new CapacityException("ID da categoria não informado.");
+            }
+
+            response.body.put("folders", capacityService.findFoldersByCategory(String.valueOf(categoryId)));
+        });
+    }
+
+    @PostMapping(value = "/get", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response get(@RequestBody Map<String, Object> body) {
+        return Response.buildResponse(response -> {
+
+            Object categoryId = body.get("id");
+            if(categoryId == null || String.valueOf(categoryId).isEmpty()) {
+                throw new CapacityException("ID da categoria não informado.");
+            }
+
+            response.body.put("category", capacityService.findCategoryById(UUID.fromString(String.valueOf(categoryId))));
+        });
+    }
+
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response create(@RequestBody Map<String, Object> body) {
+        return Response.buildResponse(response -> {
+
+            Object name = body.get("name");
+            Object image = body.get("image");
+
+            if(name == null || String.valueOf(name).isEmpty()) {
+                throw new CapacityException("Parametro name não informado.");
+            }
+
+            Category category = new Category();
+            category.setName(String.valueOf(name));
+
+            if(image != null) {
+                String imageStr = String.valueOf(image);
+                if(!imageStr.isEmpty()) {
+                    category.setImage(imageStr);
+                }
+            }
+
+            category.setAuthor(UserService.getInstance().getUserInSession().getProfile());
+
+            boolean result = capacityService.saveOrUpdateCategory(category);
+            if(!result) {
+                throw new CapacityException("Erro ao salvar o categoria.");
+            }
+
+            response.message = "Categoria criada com sucesso.";
+        });
+    }
+
+    @PostMapping(value = "/edit", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response edit(@RequestBody Map<String, Object> body) {
+        return Response.buildResponse(response -> {
+
+            Object categoryId = body.get("id");
+            if(categoryId == null || String.valueOf(categoryId).isEmpty()) {
+                throw new CapacityException("ID da categoria não informado.");
+            }
+
+            Object name =  body.get("name");
+            Object image = body.get("image");
+
+            Category category = capacityService.findCategoryById(String.valueOf(categoryId));
+            if(category == null) {
+                throw new CapacityException("Categoria não encontrado.");
+            }
+
+            if(!UserService.getInstance().isSessionOfUser(category.getAuthor().getUser())) {
+                if(!UserService.getInstance().isUserAdmin(UserService.getInstance().getUserInSession())) {
+                    throw new CapacityException("Você não tem permissão para editar esta categoria.");
+                }
+            }
+
+            if(name != null) {
+                String nameStr = String.valueOf(name);
+                if(!nameStr.isEmpty()) {
+                    category.setName(nameStr);
+                }
+            }
+            if(image != null) {
+                String imageStr = String.valueOf(image);
+                if(!imageStr.isEmpty()) {
+                    category.setImage(imageStr);
+                }
+            }
+
+            boolean result = capacityService.saveOrUpdateCategory(category);
+            if(!result) {
+                throw new CapacityException("Erro ao editar o categoria.");
+            }
+
+            response.message = "Categoria atualizada com sucesso.";
+        });
+    }
+
+    @PostMapping(value = "/delete", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response delete(@RequestBody Map<String, Object> body) {
+        return Response.buildResponse(response -> {
+
+            Object categoryId = body.get("id");
+            if(categoryId == null || String.valueOf(categoryId).isEmpty()) {
+                throw new CapacityException("ID da Categoria não informado.");
+            }
+
+            Category category = capacityService.findCategoryById(UUID.fromString(String.valueOf(categoryId)));
+            if(category == null) {
+                throw new CapacityException("Categoria não encontrada.");
+            }
+
+            if(!UserService.getInstance().isSessionOfUser(category.getAuthor().getUser())) {
+                if(!UserService.getInstance().isUserAdmin(UserService.getInstance().getUserInSession())) {
+                    throw new CapacityException("Você não tem permissão para editar esta categoria.");
+                }
+            }
+
+            boolean result = capacityService.deleteCategory(UUID.fromString(String.valueOf(categoryId)));
+            if(!result) {
+                throw new CapacityException("Erro ao deletar Categoria.");
+            }
+
+            response.message = "Categoria deletada com sucesso.";
+        });
+    }
+}
