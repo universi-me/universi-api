04e6cc4a35103333c5191f8760d4dbcd4401a9f4
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2024-03-01 08:56:54 -0300

    change: add more group types

--- src/main/java/me/universi/group/enums/GroupType.java
+++ src/main/java/me/universi/group/enums/GroupType.java
@@ -1,5 +1,8 @@
 package me.universi.group.enums;
 
+/**
+ * Enum with the types of groups that can be created
+ */
 public enum GroupType {
     INSTITUTION("Instituição"),
     CAMPUS("Campus"),
@@ -10,7 +13,17 @@ public enum GroupType {
     LABORATORY("Laboratório"),
     ACADEMIC_CENTER("Centro Acadêmico"),
     DEPARTMENT("Departamento"),
-    STUDY_GROUP("Grupo de Estudo");
+    STUDY_GROUP("Grupo de Estudos"),
+
+    GROUP_GENERAL("Grupo Geral"),
+    DIRECTORATE("Diretoria"),
+    MANAGEMENT("Gerência"),
+    COORDINATION("Coordenação"),
+    COMPANY_AREA("Área da Empresa"),
+    DEVELOPMENT_TEAM("Time de Desenvolvimento"),
+    INTEREST_GROUP("Grupo de Interesse"),
+    MISCELLANEOUS_SUBJECTS("Assuntos Diversos"),
+    ENTERTAINMENT("Entretenimento");
 	
     public final String label;
 	
