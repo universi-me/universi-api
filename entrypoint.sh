f1dda40ab908b0f356a94a6a1f629d483cc9bf9e
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2024-06-01 12:08:57 -0300

    change: use environment in log file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # send the log file to the debug URL
-if [ -n "$DEBUG_URL_POST" ] && [ -f "/spring.log" ]; then
+if [ -n "$DEBUG_URL_POST" ] && [ -f "$API_DEBUG_FILE" ]; then
     # Send the file contents via POST request
-    curl -X POST -H "Content-Type: text/plain" --data-binary @/spring.log "$DEBUG_URL_POST"
-    echo "Sent /spring.log file to $DEBUG_URL_POST"
-    rm -rf /spring.log
+    curl -k -X POST -H "Content-Type: text/plain" --data-binary @"$API_DEBUG_FILE" "$DEBUG_URL_POST"
+    echo "Sent $API_DEBUG_FILE file to $DEBUG_URL_POST"
+    rm -rf "$API_DEBUG_FILE"
 fi
 
 # Execute the Java application
