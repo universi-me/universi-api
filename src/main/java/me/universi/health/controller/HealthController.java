b70918cacfdd11f712828aa4db16b85fa94446d1
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-08-25 18:48:01 -0300

    change: database health check

--- src/main/java/me/universi/health/controller/HealthController.java
+++ src/main/java/me/universi/health/controller/HealthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.EntityManager;
 import jakarta.validation.constraints.NotNull;
 import me.universi.health.dto.HealthResponseDTO;
 
@@ -15,16 +16,31 @@ import me.universi.health.dto.HealthResponseDTO;
     produces = MediaType.APPLICATION_JSON_VALUE
 )
 public class HealthController {
+    private EntityManager entityManager;
+
+    public HealthController(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @GetMapping( "/api" )
     public @NotNull HealthResponseDTO apiHealth() {
         // If this is running then API is up
         return new HealthResponseDTO(true, null);
     }
 
-    @GetMapping( "/postgresql" )
-    public @NotNull HealthResponseDTO postgreSqlHealth() {
-        // todo
-        return new HealthResponseDTO(true, null);
+    @GetMapping( "/database" )
+    public @NotNull HealthResponseDTO databaseHealth() {
+        try {
+            boolean open = this.entityManager.isOpen();
+            return new HealthResponseDTO(
+                open,
+                open ? null : "Sessão não está aberta"
+            );
+        }
+
+        catch (Exception err) {
+            return new HealthResponseDTO( false, "Erro ao buscar sessão" );
+        }
     }
 
     @GetMapping( "/mongodb" )
