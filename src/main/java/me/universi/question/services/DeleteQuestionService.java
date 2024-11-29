fe9677616dac01a8880053d0ca7b86f1feb5aafd
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-11-29 16:47:44 -0300

    change: remove Exercise and Question modules

--- src/main/java/me/universi/question/services/DeleteQuestionService.java
+++
@@ -1,8 +0,0 @@
-package me.universi.question.services;
-
-import java.util.UUID;
-
-@FunctionalInterface
-public interface DeleteQuestionService {
-    void deleteQuestion(UUID groupId, UUID exerciseId, UUID questionId);
-}
