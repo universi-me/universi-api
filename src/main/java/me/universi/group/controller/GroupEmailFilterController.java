8b5bc1701d1e002e7df40db6ef921097df1cfc3a
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2024-12-11 10:08:16 -0300

    change: adjust delete email filter endpoint

--- src/main/java/me/universi/group/controller/GroupEmailFilterController.java
+++ src/main/java/me/universi/group/controller/GroupEmailFilterController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/group/settings/email-filter")
+@RequestMapping("/api/group/settings/email-filters")
 public class GroupEmailFilterController {
 
     private final GroupEmailFilterService groupEmailFilterService;
@@ -23,25 +23,22 @@ public class GroupEmailFilterController {
     }
 
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<GroupEmailFilter> email_filter_create( @Valid @RequestBody CreateEmailFilterDTO createEmailFilterDTO ) {
         return ResponseEntity.ok( groupEmailFilterService.createEmailFilter( createEmailFilterDTO ) );
     }
 
     @PatchMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<GroupEmailFilter> email_filter_update( @Valid @RequestBody UpdateEmailFilterDTO updateEmailFilterDTO ) {
         return ResponseEntity.ok( groupEmailFilterService.updateEmailFilter( updateEmailFilterDTO ) );
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<Boolean> email_filter_delete( @Valid @PathVariable @NotNull( message = "ID do filtro inválido" ) UUID id ) {
-        return ResponseEntity.ok( groupEmailFilterService.deleteEmailFilter( id ) );
+    public ResponseEntity<Void> email_filter_delete( @Valid @PathVariable @NotNull( message = "ID do filtro de email inválido" ) UUID id ) {
+        groupEmailFilterService.deleteEmailFilter( id );
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<List<GroupEmailFilter>> email_filter_list( @Valid @PathVariable @NotNull( message = "ID do grupo inválido" ) UUID id ) {
         return ResponseEntity.ok( groupEmailFilterService.listEmailFilter( id ) );
     }
