10dbdc952c0f3a4ce9a75ffbce1b3011fb1d3af9
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-12-17 20:04:23 -0300

    change: use PATCH instead of PUT in EducationTypeController

--- src/main/java/me/universi/education/controller/EducationTypeController.java
+++ src/main/java/me/universi/education/controller/EducationTypeController.java
@@ -11,9 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +43,7 @@ public class EducationTypeController {
         return ResponseEntity.ok( educationTypeService.findByIdOrNameOrThrow( idOrName ) );
     }
 
-    @PutMapping( path = "/{idOrName}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
+    @PatchMapping( path = "/{idOrName}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
     public ResponseEntity<EducationType> update(
         @Valid @PathVariable @NotNull String idOrName,
         @Valid @RequestBody UpdateEducationTypeDTO updateEducationTypeDTO
