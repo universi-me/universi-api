b3e3e86d2b67605d8c6ff09958e8f0a86b778ff9
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2025-03-04 15:15:11 -0300

    fix: change description back to String instead of Optional

--- src/main/java/me/universi/competence/dto/CreateCompetenceDTO.java
+++ src/main/java/me/universi/competence/dto/CreateCompetenceDTO.java
@@ -1,6 +1,5 @@
 package me.universi.competence.dto;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import jakarta.validation.constraints.Max;
@@ -12,9 +11,15 @@ public record CreateCompetenceDTO(
     @NotNull
     UUID competenceTypeId,
 
-    Optional<String> description,
+    @NotNull
+    String description,
 
     @NotNull
     @Min( Competence.MIN_LEVEL ) @Max( Competence.MAX_LEVEL )
     Integer level
-) { }
+) {
+    public CreateCompetenceDTO {
+        if ( description == null )
+            description = "";
+    }
+}
