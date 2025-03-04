9397ff7b4b8494bc12c82ac84ef2cd11ca341c21
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2025-03-04 15:00:56 -0300

    fix: set description field as optional

--- src/main/java/me/universi/competence/dto/CreateCompetenceDTO.java
+++ src/main/java/me/universi/competence/dto/CreateCompetenceDTO.java
@@ -1,10 +1,10 @@
 package me.universi.competence.dto;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import me.universi.competence.entities.Competence;
 
@@ -12,8 +12,7 @@ public record CreateCompetenceDTO(
     @NotNull
     UUID competenceTypeId,
 
-    @NotBlank
-    String description,
+    Optional<String> description,
 
     @NotNull
     @Min( Competence.MIN_LEVEL ) @Max( Competence.MAX_LEVEL )
