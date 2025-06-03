2a32de535927471d9a5b505a0662dabf8649f279
Julio Verne <julio.verne@dcx.ufpb.br>
2025-06-03 16:19:38 -0300

    change: better handle error in get image

--- src/main/java/me/universi/image/repositories/ImageMetadataRepository.java
+++ src/main/java/me/universi/image/repositories/ImageMetadataRepository.java
@@ -11,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ImageMetadataRepository extends JpaRepository<ImageMetadata, UUID> {
     Optional<ImageMetadata> findFirstByFilenameAndImageStore( String filename, ImageStoreLocation storeType );
+    Optional<ImageMetadata> findFirstByFilename( String filename );
 }
