d9a8fb3a2b412eb62020422379fb1a664b72566e
Julio Verne <julio.verne@dcx.ufpb.br>
2025-06-23 16:24:04 -0300

    change: adjust filter

--- src/main/java/me/universi/user/services/JWTFilter.java
+++ src/main/java/me/universi/user/services/JWTFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import me.universi.user.entities.User;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,24 +16,27 @@ public class JWTFilter extends OncePerRequestFilter {
     private final JWTService jwtService;
     private final AuthenticationManager authenticationManager;
 
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String AUTHENTICATION_SCHEME = "Bearer ";
+
     public JWTFilter(JWTService jwtService, AuthenticationManager authenticationManager) {
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
 
-        String header = request.getHeader("Authorization");
+        String header = request.getHeader(AUTH_HEADER);
         
         // prefer check first if user is logged-in on session, else use JWT token to authenticate
-        if(!jwtService.ENABLED || header == null || !header.startsWith("Bearer ") || LoginService.getInstance().userIsLoggedIn()) {
+        if(!jwtService.ENABLED || header == null || !header.startsWith(AUTHENTICATION_SCHEME) || LoginService.getInstance().userIsLoggedIn()) {
             filterChain.doFilter(request, response);
             return;
         }
 
         try {
-            User user = jwtService.getUserFromToken(header.replace("Bearer ", ""));
+            User user = jwtService.getUserFromToken(header.substring(7)); // remove "Bearer " prefix
             LoginService.getInstance().configureSessionForUser(user, authenticationManager);
         } catch (Exception e) {
             SecurityContextHolder.clearContext();
