ba15344b8c093feeb3a601be428872117b0096e3
Selene Haruno <selene.haruno.ss@gmail.com>
2025-08-26 01:12:00 -0300

    feat: add Role to Swagger

--- src/main/java/me/universi/role/dto/ProfileRoleDTO.java
+++ src/main/java/me/universi/role/dto/ProfileRoleDTO.java
@@ -1,10 +1,11 @@
 package me.universi.role.dto;
 
-import jakarta.annotation.Nullable;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import me.universi.profile.entities.Profile;
 import me.universi.role.entities.Role;
 
+@Schema( description = "Response body for listing Group participants with their Roles" )
 public record ProfileRoleDTO(
     @NotNull
     Profile profile,
--- src/main/java/me/universi/role/dto/UpdateRoleDTO.java
+++ src/main/java/me/universi/role/dto/UpdateRoleDTO.java
@@ -2,19 +2,27 @@ package me.universi.role.dto;
 
 import java.util.Map;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AdditionalPropertiesValue;
 import jakarta.annotation.Nullable;
 import me.universi.role.enums.FeaturesTypes;
+import me.universi.role.openapi.FeatureToLevelSchema;
 
+@Schema( description = "Request body for updating Roles in a Group" )
 public record UpdateRoleDTO(
     @Nullable
+    @Schema( description = "New name for the edited Role", examples = { "Leader", } )
     String name,
 
     @Nullable
+    @Schema( description = "New description for the edited Role", deprecated = true )
     String description,
 
     @Nullable
+    @Schema( deprecated = true )
     String group,
 
     @Nullable
+    @Schema( description = "The new Permissions of this Role", ref = FeatureToLevelSchema.REF_STRING, additionalProperties = AdditionalPropertiesValue.FALSE )
     Map<FeaturesTypes, Integer> features
 ) {}
